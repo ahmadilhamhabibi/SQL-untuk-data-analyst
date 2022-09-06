4c3421ed4eb134fb3381215ace5984456450b12c
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-09-06 17:36:12 +0700

    Update select-condition.sql

--- select-condition.sql
+++ select-condition.sql
@@ -3,8 +3,8 @@ SELECT
 	segment,
 	age,
 	CASE
-		WHEN age BETWEEN 17 AND 30 THEN 'Young Adults'
-		WHEN age BETWEEN 31 AND 45 THEN 'Middle-aged Adults'
-		WHEN age > 45 THEN 'Old Adults'
+		WHEN age BETWEEN 17 AND 30 THEN 'Young-Adults'
+		WHEN age BETWEEN 31 AND 45 THEN 'Middle-Age'
+		WHEN age > 45 THEN 'Senior'
 	END AS age_group
 FROM customer;
