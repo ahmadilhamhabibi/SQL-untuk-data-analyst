5d290882253e1ba63d576d09c240d9a52fd83208
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-09-07 21:10:25 +0700

    Create conversion.sql

---
+++ conversion.sql
@@ -0,0 +1,25 @@
+-- Conversion Data Types
+
+-- Conversion to string
+SELECT sales, TO_CHAR(sales, '9999.99') AS CHAR
+FROM sales;
+
+SELECT sales, TO_CHAR(sales, 'L9999.99') AS CURRENT
+FROM sales;
+
+SELECT order_date, TO_CHAR(order_date, 'MM-DD-YY') AS TEXT_DATE
+FROM sales;
+
+SELECT order_date, TO_CHAR(order_date, 'Month DD, YYYY') AS long_text_date_format
+FROM sales;
+
+-- Conversion to date
+SELECT TO_DATE('2014/04/14','YYYY/MM/DD');
+
+-- SELECT TO_DATE('033114', 'MM-DD-YY');
+
+
+-- Conversion to number
+SELECT TO_NUMBER('1234.5678','9999.99');
+
+SELECT TO_NUMBER('$1,234.567', 'L9,999.99');
