aabdc61ff04b5044583dff9d3e7e08ca59c734e6
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-09-13 10:08:28 +0700

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -18,12 +18,7 @@ ORDER BY customer_id;
 	
 -- INNER JOIN
 SELECT
-	s.order_line,
-	s.product_id,
-	s.customer_id,
-	s.sales,
-	cus.customer_name,
-	cus.age
+	s.order_line, s.product_id, s.customer_id, s.sales,	cus.customer_name, cus.age
 FROM sample_sales AS s
 INNER JOIN sample_customer AS cus
 ON s.customer_id = cus.customer_id
@@ -32,12 +27,7 @@ ORDER BY s.customer_id;
 
 -- LEFT JOIN
 SELECT
-	s.order_line,
-	s.product_id,
-	s.customer_id,
-	s.sales,
-	cus.customer_name,
-	cus.age
+	s.order_line, s.product_id, s.customer_id, s.sales,	cus.customer_name, cus.age
 FROM sample_sales AS s
 LEFT JOIN sample_customer AS cus
 ON s.customer_id = cus.customer_id
@@ -45,12 +35,7 @@ ORDER BY s.customer_id;
 
 -- RIGHT JOIN
 SELECT
-	s.order_line,
-	s.product_id,
-	s.customer_id,
-	s.sales,
-	cus.customer_name,
-	cus.age
+	s.order_line, s.product_id, s.customer_id, s.sales,	cus.customer_name, cus.age
 FROM sample_sales AS s
 RIGHT JOIN sample_customer AS cus
 ON s.customer_id = cus.customer_id
@@ -59,12 +44,7 @@ ORDER BY s.customer_id;
 
 -- FULL JOIN
 SELECT
-	s.order_line,
-	s.product_id,
-	s.customer_id,
-	s.sales,
-	cus.customer_name,
-	cus.age
+	s.order_line, s.product_id, s.customer_id, s.sales,	cus.customer_name, cus.age
 FROM sample_sales AS s
 FULL JOIN sample_customer AS cus
 ON s.customer_id = cus.customer_id
