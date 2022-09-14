8ae0bb9bf63e69c87500d23c29e97d2a4543b1c6
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-09-14 10:01:46 +0700

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,21 +1,47 @@
--- VIEW
+-- Virtual Table
 
-CREATE VIEW logistics AS
-SELECT a.order_line,
-		a.order_id,
-		b.customer_name,
-		b.city,
-		b.state,
-		b.country
+-- CTE 
+WITH CTE_Logistic AS 
+ (SELECT a.order_line, a.order_id, b.customer_name, b.city, b.state, b.country
+ FROM sales AS a
+ LEFT JOIN customer AS b
+ ON a.customer_id = b.customer_id
+ ORDER BY a.order_id)
+SELECT order_id, customer_name, city
+FROM CTE_Logistic
+
+-- TEMP TABLE
+DROP TABLE IF EXISTS Temp_logistic;
+
+CREATE TEMP TABLE Temp_logistic (
+order_line INT, 
+order_id VARCHAR(50), 
+customer_name VARCHAR(50), 
+city VARCHAR(50), 
+state VARCHAR(50), 
+country VARCHAR(50)
+)
+
+INSERT INTO Temp_logistic 
+SELECT a.order_line, a.order_id, b.customer_name, b.city, b.state, b.country
 FROM sales AS a
 LEFT JOIN customer AS b
 ON a.customer_id = b.customer_id
 ORDER BY a.order_id;
 
--- UPDATE
-UPDATE logistics
-	SET Country = 'US'
-	WHERE Country='United States';
+SELECT * FROM Temp_logistic
+
+
+-- VIEW
+
+CREATE VIEW logistics AS
+SELECT a.order_line, a.order_id, b.customer_name, b.city, b.state, b.country
+FROM sales AS a
+LEFT JOIN customer AS b
+ON a.customer_id = b.customer_id
+ORDER BY a.order_id;
+
+SELECT * FROM logistics
 
 
 -- DROP
